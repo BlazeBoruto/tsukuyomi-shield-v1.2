c5240d7363c71463c91c42c6aa10d55a43fadcb1
BlazeBoruto <65187201+BlazeBoruto@users.noreply.github.com>
2023-12-27 18:24:06 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     app: {
         px: '+',
-        token: 'OTI2NjgwMDcxOTE2MzU1NjA1.GY9Bjv.pUq2bk3c7QtM8cq0UywF_dcpOzihKqKM0NX8C8',
+        token: '###',
         owners: '583064092349169840',
         funny: '583064092349169840',
         color: '#291a99',
-        footer: 'isekai lpb',
+        footer: 'test',
         maxserver: '6',
         everyoneMention: false,
         hostedBy: true,
